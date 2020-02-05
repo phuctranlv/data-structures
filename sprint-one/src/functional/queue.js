f2f765fc174c78003e25752fb215846a51af570f
Phuc Tran <phuctranlv@gmail.com>
2020-02-05 13:17:27 -0800

    Complete a function to create Queue objects using functional style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var position = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[position] = value;
+    position += 1;
   };
 
   someInstance.dequeue = function() {
+    var currentPositionArray = Object.keys(storage).map(element => Number.parseInt(element));
+    var itemToReturn = storage[Math.min(... currentPositionArray)]
+    delete storage[Math.min(... currentPositionArray)];
+    return itemToReturn;
+    
   };
 
   someInstance.size = function() {
+    // debugger;
+    var currentPositionArray = Object.keys(storage).map(element => Number.parseInt(element));
+    // debugger;
+    return currentPositionArray.length;
   };
 
   return someInstance;
 };
+var coffee = Queue()
+coffee.enqueue('black')
