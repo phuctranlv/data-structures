9172e3441d41f5c84d4ada7c47a5bdc728639c17
Phuc Tran <phuctranlv@gmail.com>
2020-02-05 14:53:32 -0800

    Create a maker function for a stack using function style

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var position = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    position += 1;
+    storage[position] = value;
   };
 
   someInstance.pop = function() {
+    var positionArray = Object.keys(storage).map(element => Number.parseInt(element));
+    var highestValue = Math.max(... positionArray);
+    var popItem = storage[highestValue];
+    delete storage[highestValue];
+    return popItem;
   };
 
   someInstance.size = function() {
+    var keysArray = Object.keys(storage);
+    return keysArray.length;
   };
 
   return someInstance;
