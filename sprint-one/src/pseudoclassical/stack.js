972085c5cb659af689a0ae1b237cb74f4251b420
Phuc Tran <phuctranlv@gmail.com>
2020-02-06 11:31:52 -0800

    Create a Stack classs using pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.position = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.position += 1;
+  this.storage[this.position] = value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.position !== 0) {
+    var output = this.storage[this.position];
+    delete this.storage[this.position];
+    this.position -= 1;
+    return output;
+  }
+
+};
+
+Stack.prototype.size = function() {
+  return this.position;
+};
