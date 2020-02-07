f60bdae2dddd4c405fd0779830acd8e126e550d3
phuctranlv <phuctranlv@gmail.com>
2020-02-07 10:27:28 -0800

    set ut pseudocode and .addtotail() function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // making a new node "invoke our node function"
+    var newNode = Node(value);
+    // add the new node to the list object
+    debugger;
+    if (!newAddress) {
+      var newAddress = 0;
+    }
+    newAddress += 1;
+    list[newAddress] = newNode;
+    // update the lable that says tail to point to the newely created node "node.next"
+    if (list.tail) {
+      list.tail.next = newAddress; // updating the next property inside of the old node
+    }
+    // update the old tail to point to the new tail
+    list.tail = newNode;
+
+
   };
 
   list.removeHead = function() {
   };
 
   list.contains = function(target) {
+
   };
 
   return list;
@@ -27,3 +45,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//  var something = LinkedList() ---> {}
