09b29cbd4a8a31a91aa59dcd2595665114482f6d
phuctranlv <phuctranlv@gmail.com>
2020-02-07 20:10:08 -0800

    Complete linkedList data structure

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,6 +3,7 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
   list.newAddress = 0;
+  list.headAddress = 0;
 
   list.addToTail = function(value) {
     // making a new node "invoke our node function"
@@ -12,17 +13,19 @@ var LinkedList = function() {
       var isThisTheVeryFirstNode = true;
 
     }
+
+
     this.newAddress += 1;
-    list[this.newAddress] = newNode;
+    this[this.newAddress] = newNode;
     // update the lable that says tail to point to the newely created node "node.next"
     if (list.tail) {
-      list.tail.next = this.newAddress; // updating the next property inside of the old node
+      this.tail.next = this.newAddress; // updating the next property inside of the old node
     }
     // update the old tail to point to the new tail
-    list.tail = newNode;
+    this.tail = newNode;
     if (isThisTheVeryFirstNode) {
-      list.head = newNode;
-      debugger;
+      this.headAddress = this.newAddress;
+      this.head = newNode;
       isThisTheVeryFirstNode = false;
     }
   };
@@ -34,25 +37,38 @@ var LinkedList = function() {
 
     // we don't need to re-assign names for our properties
 
-
     // set variable to equal the value of the current head node that we are going to delete
     let output = list.head.value;
-    debugger;
     // Set list.head to reference the value of node.next in the current head node
-    var nextHeadAddress = list.head.next;
+    var nextHeadAddress = this.head.next;
     // delete first node in the list
-    debugger;
-    delete list.head;
-    debugger;
+
+    delete this[this.headAddress];
+    // * we need to delete the original head object inside the list object *
+
     // set new head
-    list.head = list[nextHeadAddress];
-    debugger;
+    this.head = list[nextHeadAddress];
     // return the variable that contains the deleted value
     return output;
 
   };
 
   list.contains = function(target) {
+    // create a function that returns a boolean if a certain value is contained in our linked list
+
+    // we're going to take a target input and for loop through our linked list property
+
+    // we are going to compare if our target input is equal to our nodes.value property
+    for (var key in this) {
+      if (this[key].value === target) {
+        return true;
+      }
+    }
+    return false;
+    // return true if the comparison is true
+
+    // else return false
+
 
   };
 
