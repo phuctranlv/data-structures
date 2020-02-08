ddc364cfbc3278b00e8ead5a9676973e87b7d95f
phuctranlv <phuctranlv@gmail.com>
2020-02-08 11:06:07 -0800

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,40 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add method
+  // takes input item
+  // add item to _storage property object
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  // .contains checks if item is present in set
+  // takes input string
+
+  // check if input string on property = input string
+  if (this._storage[item] === item) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // return boolean if above true
 };
 
 setPrototype.remove = function(item) {
+  // remove: delete item from set
+  // input: item
+  // no output, just side effect of item being removed
+
+  if (this._storage.hasOwnProperty(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
